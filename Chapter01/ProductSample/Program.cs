ab91b403eae4367a88517030669ef1ef9d5fc172
Kounohiroyuki <oibc35138@ojs.ac.jp>
2024-04-18 13:37:23 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter01/ProductSample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSample {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Product karinto = new Product(123, "かりんとう", 180);
+            Product daihuku = new Product(777, "大福", 200);
+
+           int karintoprices = karinto.Price;
+
+            int daihukuPrice = daihuku.Price;
+            int taxIncluded = karinto.GetPriceIncludingTax();//税込の金額
+            int daihukutaxIncluded = daihuku.GetPriceIncludingTax();
+
+            Console.WriteLine(karinto.Name + "の税込金額は"+taxIncluded+"円【"+ karintoprices + "円】") ;
+            Console.WriteLine(daihuku.Name + "の税込金額は" + daihukutaxIncluded + "円【" + daihukuPrice + "円】");
+
+       
+        }
+    }
+}
---
+++ Chapter01/Sample_p38/Main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_p38 {
+    internal class Program {
+        static void Main(string[] args) {
+            Employee employee = new Employee {
+                Id = 100,
+                Name = "山田太郎",
+                Birthday = new DateTime(1992, 4, 5),
+                Divisionname = "第一営業部",
+            };
+            Console.WriteLine("{}({})は、{}に所属しています。",
+                                employee.Name, employee.GetAge(), employee.Divisionname);
+
+            Console.WriteLine("{0} - {1}年");
+
+
+
+            Person person = Student;
+            if (person is Student)
+                Console.WriteLine("person is Student");
+
+            object obj = Student;
+            if (obj is Student)
+                Console.WriteLine("obj is Student");
+        }
+    }
+}
