49543f911d8e32c45c3f7035b30fd05371086a1d
Kounohiroyuki <oibc35138@ojs.ac.jp>
2024-04-24 14:08:48 +0900

    メソッドの移動

--- Chapter02/SalesCounter/Program.cs
+++ Chapter02/SalesCounter/Program.cs
@@ -8,33 +8,12 @@ using System.Threading.Tasks;
 namespace SalesCounter {
     internal class Program {
         static void Main(string[] args) {
-            //戻り値を受け取る
-            List<Sale> sales = ReadSales(@"C:\\Users\\infosys\\source\\repos\\OOP2024\\Chapter02\\SalesCounter\\date\\Sales.csv");
-            //戻り値のコレクションを一件ずつ出力する
-            foreach ( Sale  sale in sales) {
-                // Console.WriteLine(sale.shopName+""+sale.ProductCategory+""+sale.Amount);
-               //  Console.WriteLine("店名：{0} カテゴリー：{1} 売上：{2}",sale.shopName,sale.ProductCategory,sale.Amount);
-                Console.WriteLine($"店名：{sale.shopName} カテゴリー：{sale.ProductCategory} 売上：{sale.Amount}");
-            }                              
-        }
 
-        //売上データを読み込みSaleオブジェクトのリストを返す
-        static List<Sale> ReadSales(string filePath) {
-        List<Sale> sales = new List<Sale>();
-            string[] lines = File.ReadAllLines(filePath);
-            foreach(string line in lines) {
-                string[] items = line.Split(',');
-                Sale sale = new Sale {
-                    shopName = items[0],
-                    ProductCategory = items[1],
-                    Amount = int.Parse(items[2]),
-
-                };
-                sales.Add(sale);
-
+            SalesCounter sales = new SalesCounter(@"date\Sales.csv");
+            Dictionary<string,int>amountPerStore = sales.GetPerStoreSales();
+            foreach (KeyValuePair<string,int>obj in amountPerStore) {
+                Console.WriteLine("{0} {1}",obj.Key,obj.Value);
             }
-            return sales;
-
         }
     }
 }
