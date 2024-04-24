fd67232936c77607544c92a60ceb74d0bdf3b8c0
Kounohiroyuki <oibc35138@ojs.ac.jp>
2024-04-24 15:17:36 +0900

    2.1.1完成

---
+++ Chapter02/演習問題/Exercise1/Song.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    internal class Song {
+        public string Title { get; set; }
+        public string ArtistName { get; set; }
+        public int Lemgth { get; set; }
+    }
+}
