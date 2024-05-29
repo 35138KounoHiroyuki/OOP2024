621090026180c38a35188aa7cecf31bfb0586aec
Kounohiroyuki <oibc35138@ojs.ac.jp>
2024-05-29 15:44:51 +0900

    5.1

---
+++ Chapter05/Exercise01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var st1 = Console.ReadLine();
+            var st2 = Console.ReadLine();
+            if (String.Compare(st1, st2, true) == 0)
+                Console.WriteLine("ひとしい");
+            else 
+                Console.WriteLine("等しくない");
+        }
+    }
+}
