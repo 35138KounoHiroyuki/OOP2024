02ecdbbe1626f3ab44041267a5214e12a32b74b1
Kounohiroyuki <oibc35138@ojs.ac.jp>
2024-06-28 10:34:27 +0900

    2024  6/28 Car

---
+++ FormApps/CarReportSystem/Form1.cs
@@ -0,0 +1,43 @@
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Metrics;
+
+namespace CarReportSystem {
+    public partial class Form1 : Form {
+
+        //カーレポート管理用リスト
+        BindingList<CarReport> listCarReports = new BindingList<CarReport>();
+
+        //コンストラクタ
+        public Form1() {
+            InitializeComponent();
+            dgvCarReport.DataSource = listCarReports;
+        }
+
+        private void btAddReport_Click(object sender, EventArgs e) {
+            CarReport carReport = new CarReport {
+                Date = dtpDate.Value,
+                Auther = cbAuther.Text,
+               Maker = GetRadioButtonMaker(),
+                CarName = cbCarName.Text,
+                Report = tbReport.Text,
+                Picture =pbPicture.Image,
+            };
+            listCarReports.Add(carReport);
+            
+        }
+        private CarReport.MakerGroup GetRadioButtonMaker() {
+            if (rbToyota.Checked)
+                return CarReport.MakerGroup.トヨタ;
+            else if (rbNissan.Checked)
+                return CarReport.MakerGroup.日産;
+            else if (rbHonda.Checked)
+                return CarReport.MakerGroup.ホンダ;
+            else if (rbSubaru.Checked)
+                return CarReport.MakerGroup.スバル;
+            else if (rbImport.Checked)
+                return CarReport.MakerGroup.輸入車;
+            else return CarReport.MakerGroup.その他;
+        }
+    }
+}
