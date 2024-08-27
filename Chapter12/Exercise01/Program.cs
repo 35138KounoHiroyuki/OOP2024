38df8e8a63661358407ff954048d3ff088292027
Kounohiroyuki <oibc35138@ojs.ac.jp>
2024-08-27 11:20:05 +0900

    12.1

---
+++ Chapter12/Exercise01/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Exercise1_1("employee.xml");
+
+            // これは確認用
+            Console.WriteLine(File.ReadAllText("employee.xml"));
+            Console.WriteLine();
+
+            Exercise1_2("employees.xml");
+            Exercise1_3("employees.xml");
+            Console.WriteLine();
+
+            Exercise1_4("employees.json");
+
+            // これは確認用
+            Console.WriteLine(File.ReadAllText("employees.json"));
+        }
+
+        private static void Exercise1_1(string v) {
+            var emp = new Employee {
+                Id = 123,
+                Name = "X",
+                HireDate = new DateTime(2001, 5, 10)
+            };
+            using (var writer = XmlWriter.Create("employee.xml")) {
+                var serializer = new XmlSerializer(emp.GetType());
+                serializer.Serialize(writer, emp);
+               
+         }
+        }
+
+        private static void Exercise1_2(string v) {
+           
+        }
+
+        private static void Exercise1_3(string v) {
+           
+        }
+
+        private static void Exercise1_4(string v) {
+           
+        }
+    }
+}
