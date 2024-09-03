22474707a1e634222a64bd9d47204aa4ccca73fd
Kounohiroyuki <oibc35138@ojs.ac.jp>
2024-09-03 11:24:39 +0900

    タイトル表示

---
+++ FormApps/RssReader/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace RssReader {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btGet_Click(object sender, EventArgs e) {
+            using (var wc = new WebClient()) {
+                var url = wc.OpenRead(tbRssUrl.Text);
+                var xdoc = XDocument.Load(url);
+
+                var xelements = xdoc.Root.Descendants("item");
+                foreach (var item in xelements) {
+                    XElement xname = item.Element("item")
+                                          .Elements("title")
+                                          .Select(x => x.);
+                                          
+                    lbRssTitle.Items.Add();
+
+
+               }
+            }
+        }
+    }
+}
