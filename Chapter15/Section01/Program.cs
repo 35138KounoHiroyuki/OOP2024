b6f440b94236f038fd13f1eb412976f666d627c7
Kounohiroyuki <oibc35138@ojs.ac.jp>
2024-09-30 14:37:00 +0900

    RssReader完成

---
+++ Chapter15/Section01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+
+            var group = Library.Books
+                        .GroupBy(b => b.PublishedYear)
+                        .OrderBy(g => g.Key);
+           
+            foreach (var g in group)
+            {
+                Console.WriteLine($"{g.Key}年");
+            foreach (var book in g) {
+                Console.WriteLine($"   {book}");
+                }
+            }
+
+        }
+    }
+}
---
+++ Chapter15/演習問題/exercise01/Program.cs
@@ -0,0 +1,67 @@
+﻿using Section01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Exercise1_2();
+            Console.WriteLine();
+            Exercise1_3();
+            Console.WriteLine();
+            Exercise1_4();
+            Console.WriteLine();
+            Exercise1_5();
+            Console.WriteLine();
+            Exercise1_6();
+            Console.WriteLine();
+            Exercise1_7();
+            Console.WriteLine();
+            Exercise1_8();
+
+            Console.ReadLine();
+        }
+
+        private static void Exercise1_2() {
+            var max = Library.Books.Max(b => b.Price);
+            var book = Library.Books.Where(b => b.Price == max);
+            foreach (var books in book)
+            {
+                Console.WriteLine(books);
+            }
+           
+        }
+
+        private static void Exercise1_3() {
+            var query = Library.Books.GroupBy(b => b.PublishedYear)
+                               .Select(g => new { PublishedYear = g.Key, Count = g.Count() })
+                               .OrderBy(x => x.PublishedYear);
+            foreach (var item in query) {
+                Console.WriteLine("{0}年 {1}冊", item.PublishedYear, item.Count);
+            }
+        }
+
+        private static void Exercise1_4() {
+            throw new NotImplementedException();
+        }
+
+        private static void Exercise1_5() {
+            throw new NotImplementedException();
+        }
+
+        private static void Exercise1_6() {
+            throw new NotImplementedException();
+        }
+
+        private static void Exercise1_7() {
+            throw new NotImplementedException();
+        }
+
+        private static void Exercise1_8() {
+            throw new NotImplementedException();
+        }
+    }
+}
