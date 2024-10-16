6c4b371a864bd7bdc33faf4e30d42507c85edac1
Kounohiroyuki <oibc35138@ojs.ac.jp>
2024-10-16 13:57:11 +0900

    17_1

---
+++ Chapter17/TemplateMethod/TextNumberSizeChange/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFileProcessor;
+using TextNumberChange;
+
+
+
+namespace TextNumberChanger {
+    internal class Program {
+        static void Main(string[] args) {
+            TextProcessor.Run<TextNumberChangeProcessor>(args[0]);
+        }
+    }
+}
