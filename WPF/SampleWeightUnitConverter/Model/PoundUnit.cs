687287344196ba7c5baccb8153e4f48d4c5672a2
Kounohiroyuki <oibc35138@ojs.ac.jp>
2024-11-06 15:58:41 +0900

    gramunit

---
+++ WPF/SampleWeightUnitConverter/Model/PoundUnit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleWeightUnitConverter.Model {
+    //ポンド単位を表すクラス
+    public class PoundUnit :WeightUnit{
+        private static List<PoundUnit> units = new List<PoundUnit> {
+          new PoundUnit{Name = "oz",Coefficient = 1,},
+          new PoundUnit{Name = "lb",Coefficient = 16,},
+          new PoundUnit{Name = "st",Coefficient = 16*14,},
+        };
+        public static ICollection<PoundUnit> Units { get { return units; } }
+        /// <summary>
+        /// ヤード単位からメートル単位に変換します
+        /// </summary>
+        /// <param name="unit">ヤード単位</param>
+        /// <param name="value">値</param>
+        /// <returns></returns>
+        public double FromGramUnit(GramUnit unit, double value) {
+            return (value * unit.Coefficient) * 25.4 / this.Coefficient;
+        }
+
+    }
+}
